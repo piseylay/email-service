78e6edd36516784b3ad490c5f98e14f0d037faf2
pisey_lay <piseylay12@gmail.com>
2022-03-28 11:12:20 +0700

    Initial commit

---
+++ src/test/kotlin/com/ig/email/EmailApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ig.email
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
